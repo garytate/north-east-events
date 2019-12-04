03bdacc61e39e31b59948437beb6ff0579124be9
Gary Tate <me@garytate.co.uk>
2019-12-04 21:48:56 +0000

    Added price

--- js/events.js
+++ js/events.js
@@ -9,7 +9,7 @@ window.onload = function eventShowcase() {
       for (var i = 0; i < _data.length; i++) {
         console.log(_data[i].name);
         var eventText = eventText.concat(
-          `<div class="event-card"><h1>${_data[i].name}</h1><p>${_data[i].desc}</p>`
+          `<div class="event-card"><h1>${_data[i].name}</h1><p>${_data[i].desc}</p><p>${_data[i].price}</p></div>`
         );
       }
       console.log(eventText);
