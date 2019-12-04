716cd592afb3f87634a1b8db7519bde2325c7ebd
Gary Tate <me@garytate.co.uk>
2019-12-04 21:13:06 +0000

    Outputs proper html

--- js/events.js
+++ js/events.js
@@ -1,15 +1,18 @@
 window.onload = function eventShowcase() {
-  document.getElementById('events').innerHTML =
-    "<div class='event-card'><p>Hello</p></div>";
+  fetch('../json/events.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var eventText = '';
+      var _data = data.events;
+      for (var i = 0; i < _data.length; i++) {
+        console.log(_data[i].name);
+        var eventText = eventText.concat(
+          `<div class="event-card"><h1>${_data[i].name}</h1><p>${_data[i].desc}</p>`
+        );
+      }
+      console.log(eventText);
+      document.getElementById('events').innerHTML = eventText;
+    });
 };
-
-fetch('../json/events.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    var _data = data.events;
-    for (var i = 0; i < _data.length; i++) {
-      console.log(_data[i].name);
-    }
-  });
