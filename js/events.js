6a5cee9d70a3eee8c45f9d733290fad39d75c60f
Gary Tate <me@garytate.co.uk>
2019-12-05 06:32:08 +0000

    Removed debug console.log

--- js/events.js
+++ js/events.js
@@ -7,12 +7,10 @@ window.onload = function eventShowcase() {
       var eventText = '';
       var _data = data.events;
       for (var i = 0; i < _data.length; i++) {
-        console.log(_data[i].name);
         var eventText = eventText.concat(
           `<div class="event-card"><h1>${_data[i].name}</h1><p>${_data[i].desc}</p><p>${_data[i].price}</p></div>`
         );
       }
-      console.log(eventText);
       document.getElementById('events').innerHTML = eventText;
     });
 };
