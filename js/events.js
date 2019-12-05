d3f23c21cb3de183f568757cfa725553e89ffa0b
Gary Tate <me@garytate.co.uk>
2019-12-05 06:56:42 +0000

    Added an image to the auto-js

--- js/events.js
+++ js/events.js
@@ -8,7 +8,12 @@ window.onload = function eventShowcase() {
       var _data = data.events;
       for (var i = 0; i < _data.length; i++) {
         var eventText = eventText.concat(
-          `<div class="event-card"><h1>${_data[i].name}</h1><p>${_data[i].desc}</p><p>${_data[i].price}</p></div>`
+          `<div class="event-card">
+            <img src=${_data[i].image}>
+            <h1>${_data[i].name}</h1>
+            <p>${_data[i].desc}</p>
+            <p>${_data[i].price}</p>
+          </div>`
         );
       }
       document.getElementById('events').innerHTML = eventText;
