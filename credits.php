8f53e98da71410a5b31b805223707fffbd16f08f
Gary Tate <me@garytate.co.uk>
2020-03-26 10:55:22 +0000

    Began work on the credits page.

---
+++ credits.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Meta Descriptions & Setup -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- SEO Tags -->
+    <meta name="author" content="Gary Tate">
+    <meta name="description" content="Come join us for updates on the best events in the North East of England!">
+    <meta name="keywords" content="newcastle, north east, events, tickets, things to do">
+
+    <!-- Imported links -->
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/events.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/5c6ddc6d04.js" crossorigin="anonymous"></script>
+
+    <!-- Website Title -->
+    <title>North East Events &bull; Credits</title>
+
+</head>
+
+<body>
+
+    <!-- Top Bar, Navigation + Title -->
+    <header>
+
+        <img src="./assets/logo.svg" class="logo">
+
+        <!-- Navigation *Right-Aligned* -->
+        <nav>
+            <a href="index.php" >Home</a>
+            <a href="events.php">Events</a>
+            <a href="admin.php">Admin</a>
+            <a href="credits.php">Credits</a>
+            <a href="./assets/pdf/Web Dev Assignment 1.pdf">Design</a>
+            <a href="https://github.com/garytate/north-east-events"><i class="fab fa-github"></i></a>
+        </nav>
+    </header>
+
+	<section class="credits">
+		<div class="container">
+		<h1>Information</h1>
+		<p>Gary Tate<br>W17001980<br>me@garytate.co.uk</p>
+		<h1>Acknowledgements</h1>
+		<p><b>Company Logo</b> - Gary Tate | me@garytate.co.uk</p>
+		</div>
+	</section>
+
+    <footer>
+        <p>North East Events &copy; 2019</p>
+    </footer>
+
+</body>
+
+</html>
+</html>
